966d3583f17ebc6d8fde98c3419c9fd87b4654be
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-26 17:28:42 +0000

    add nav structure

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,5 +1,22 @@
-const Nav = () => {
-  return <div></div>;
+import styles from './../styles/Nav.module.scss';
+
+const Nav = ({ isLoggedIn }) => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>music library</h2>
+      {isLoggedIn ? (
+        <div className={styles['btn-container']}>
+          <button className={styles.btn}>my account</button>
+          <button className={styles.btn}>logout</button>
+        </div>
+      ) : (
+        <div className={styles['btn-container']}>
+          <button className={styles.btn}>sign up</button>
+          <button className={styles.btn}>login</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Nav;
