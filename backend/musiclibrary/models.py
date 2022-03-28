4ac85812dbe8af749c2b90597af1ce6b6f963a54
sgs22 <samuel_smith@outlook.com>
2022-03-28 22:30:41 +0100

    update backend model field requirements

--- backend/musiclibrary/models.py
+++ backend/musiclibrary/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
-    length = models.IntegerField()
-    year = models.DateField()
+    genre = models.CharField(max_length=200, blank=True, null=True)
+    length = models.IntegerField(blank=True, null=True)
+    year = models.DateField(blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
