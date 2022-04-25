7e8ca27afe57fc9f053269ea00dbc461f319941e
sgs22 <samuel_smith@outlook.com>
2022-04-25 18:28:56 +0100

    add album model

--- backend/musiclibrary/models.py
+++ backend/musiclibrary/models.py
@@ -7,7 +7,17 @@ class Song(models.Model):
     length = models.IntegerField(blank=True, null=True)
     year = models.DateField(blank=True, null=True)
     active = models.BooleanField(default=True)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=200)
+    album_artist = models.CharField(max_length=200)
+    album_cover = models.ImageField()
+
+    def __str__(self):
+        return self.album_title
+
 
