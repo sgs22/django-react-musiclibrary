25ea8599cd8551a210fa1d0ef94fe056573d6bcc
Duarte Almeida <duartemalmeida@gmail.com>
2022-07-16 16:36:19 +0100

    recreate frontend with nextjs

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta name='description' content='' />
+        <link rel='icon' href='/' />
+      </Head>
+
+      <header></header>
+
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
