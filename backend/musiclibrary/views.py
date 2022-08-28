7bd7c5a83e612e4f2b5260218d0bc9f40499b11f
Samuel Smith <Samuel_Smith@outlook.com>
2022-08-28 13:45:48 +0200

    add tinytag for getting music metadata

--- backend/musiclibrary/views.py
+++ backend/musiclibrary/views.py
@@ -5,9 +5,13 @@ from rest_framework import viewsets
 from .serializers import TrackSerializer, AlbumSerializer, PlaylistSerializer, UserSerializer
 from .models import Track, Album, Playlist
 
+from tinytag import TinyTag
+
 class SongView(viewsets.ModelViewSet):
     serializer_class = TrackSerializer
     queryset = Track.objects.all()
+    # tag = TinyTag.get('Fly Me To The Moon (2008 Remastered).mp3')
+    # print(tag)
 
 class AlbumView(viewsets.ModelViewSet):
     serializer_class = AlbumSerializer
